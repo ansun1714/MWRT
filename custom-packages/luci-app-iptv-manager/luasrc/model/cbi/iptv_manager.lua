bcb01a33fe804cb9c44776f86fdc2161e239c381
ansun1714 <175953410@qq.com>
2026-06-14 02:22:59 +0800

    Add unicast IPTV proxy configuration options

--- custom-packages/luci-app-iptv-manager/luasrc/model/cbi/iptv_manager.lua
+++ custom-packages/luci-app-iptv-manager/luasrc/model/cbi/iptv_manager.lua
@@ -1,4 +1,4 @@
-local m, m_msd, m_rtp, s, o
+local m, m_msd, m_rtp, m_proxy, s, o
 
 -- ══ Map 1：全局控制 (/etc/config/iptv_manager) ════════════════
 m = Map("iptv_manager",
@@ -253,4 +253,101 @@ o = s:option(Flag, "video_snapshot",
 o.rmempty = true
 o.default = "0"
 
-return m, m_msd, m_rtp
+
+-- ══ Map 4：单播IPTV代理 (/etc/config/iptv_manager) ════════════
+m_proxy = Map("iptv_manager",
+    translate("单播IPTV代理"),
+    translate("将运营商限定网络下的单播直播源（HTTP格式，非组播）" ..
+        "通过本机转发，使其他网络下的设备也能播放。" ..
+        "配合 DDNS 域名可在外网访问。"))
+
+-- 全局设置
+s = m_proxy:section(NamedSection, "unicast_settings", "unicast_proxy", translate("全局设置"))
+s.addremove = false
+s.anonymous = true
+
+o = s:option(Value, "ddns_host", translate("DDNS 域名"))
+o.placeholder = "myhome.ddns.net"
+o.rmempty = true
+o.description = translate(
+    "填写用于远程访问的 DDNS 域名（不含端口和 http://）。" ..
+    "DDNS 解析需在「系统 -> 动态DNS」中单独配置好，" ..
+    "这里只是用来生成外网播放列表地址。")
+
+-- 代理规则
+s = m_proxy:section(TypedSection, "unicast_rule", translate("代理规则"))
+s.template = "cbi/tblsection"
+s.anonymous = true
+s.addremove = true
+s.sortable  = true
+
+o = s:option(Flag, "enable", translate("启用"))
+o.rmempty = false
+o.default = "1"
+
+o = s:option(Value, "name", translate("备注名称"))
+o.placeholder = "江苏联通单播"
+
+o = s:option(Value, "upstream_host", translate("源地址IP"))
+o.placeholder = "112.86.202.37"
+o.datatype = "ipaddr"
+o.rmempty = false
+
+o = s:option(Value, "upstream_port", translate("源端口"))
+o.placeholder = "8112"
+o.datatype = "port"
+o.rmempty = false
+
+o = s:option(Value, "listen_port", translate("本机监听端口"))
+o.placeholder = "8112"
+o.datatype = "port"
+o.rmempty = false
+
+o = s:option(Flag, "wan_access", translate("公网访问"))
+o.rmempty = false
+o.default = "0"
+o.description = translate(
+    "开启后自动在防火墙 WAN 区域放行该端口，" ..
+    "配合上方 DDNS 域名即可在外网访问。" ..
+    "需要运营商分配公网IP（非NAT），否则无法生效。")
+
+-- 访问地址一览
+local info = m_proxy:section(SimpleSection, translate("访问地址一览"))
+
+local sys = require "luci.sys"
+local lan_ip = sys.exec("uci -q get network.lan.ipaddr"):gsub("%s+", "")
+if lan_ip == "" then lan_ip = "192.168.1.1" end
+
+local ddns_host = m_proxy.uci:get("iptv_manager", "unicast_settings", "ddns_host") or ""
+local lines = {}
+
+m_proxy.uci:foreach("iptv_manager", "unicast_rule", function(sec)
+    if sec.enable ~= "1" then return end
+    local rname = sec.name or sec[".name"]
+    local lport  = sec.listen_port or "?"
+
+    lines[#lines+1] = "<b>" .. pcdata(rname) .. "</b>"
+    lines[#lines+1] = translate("局域网") .. ": http://" ..
+        lan_ip .. ":" .. lport .. "/原始路径"
+
+    if sec.wan_access == "1" then
+        if ddns_host ~= "" then
+            lines[#lines+1] = translate("外网") .. ": http://" ..
+                pcdata(ddns_host) .. ":" .. lport .. "/原始路径"
+        else
+            lines[#lines+1] = "<span style='color:orange'>" ..
+                translate("外网：已开启公网访问，但未填写 DDNS 域名") ..
+                "</span>"
+        end
+    end
+
+    lines[#lines+1] = "<br/>"
+end)
+
+if #lines == 0 then
+    lines[1] = translate("暂无已启用的规则")
+end
+
+info.description = table.concat(lines, "<br/>")
+
+return m, m_msd, m_rtp, m_proxy
